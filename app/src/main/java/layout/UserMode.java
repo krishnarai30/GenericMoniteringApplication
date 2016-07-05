ee19aa3dff7ed2ec3b3994e145aa8725f97a105c
krishnarai30 <krishna.rai30@yahoo.com>
2016-07-05 15:35:50 +0530

    jai shri ram... user mode semi done

--- app/src/main/java/layout/UserMode.java
+++ app/src/main/java/layout/UserMode.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.design.widget.FloatingActionButton;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import sd_dtu.genericmoniteringapplication.R;
 
@@ -14,11 +17,26 @@ import sd_dtu.genericmoniteringapplication.R;
 public class UserMode extends Fragment {
 
 
+    public Button alertbtn,databtn,actuationbtn;
+    FloatingActionButton floatingActionButton;
+    public void clicks(){
+
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_mode, container, false);
+        RelativeLayout relativeLayout=(RelativeLayout)inflater.inflate(R.layout.fragment_user_mode, container, false);
+        alertbtn=(Button)getView().findViewById(R.id.alertsbtn);
+        databtn=(Button)getView().findViewById(R.id.databtn);
+        actuationbtn=(Button)getView().findViewById(R.id.actuationbtn);
+        floatingActionButton=(FloatingActionButton)getView().findViewById(R.id.fab);
+        clicks();
+
+
+        return relativeLayout;
     }
+
+
 
 }
